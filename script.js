493e8287713da0a4f7d2e5e07654f9484817d7e8
dedicatorias-web <arpt1971@gmail.com>
2025-08-28 15:01:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -368,14 +368,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Service Worker (opcional)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-      .then(registration => {
-        console.log('ServiceWorker registrado com sucesso:', registration.scope);
-      })
-      .catch(err => {
-        console.error('Falha no registro do ServiceWorker:', err);
-      });
-  });
-}
+
